16edf987601b66aeb862dcc3c6d9da9c1e213308
Becca King <becca.king3232@gmail.com>
2019-12-26 16:09:13 -0500

    testing alt signup path

--- controllers/users.php
+++ controllers/users.php
@@ -6,8 +6,6 @@ if($_REQUEST['action'] === 'post'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $new_user = new User(null, $body_object->username, $body_object->password);
-  session_start();
-  $_SESSION["username"] = $new_user->username;
-  echo json_encode($_SESSION["username"]);
+  echo json_encode($new_user->username);
 }
 ?>
